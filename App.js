ac3e8c71233acc12ad5c34897002c7de096af9fb
jackthomascarr <71611362+jackthomascarr@users.noreply.github.com>
2022-04-24 21:35:37 -0600

    clock component

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import ClockComponent from './clock';
 
 export default function App() {
+  const [started, setStarted] = useState(false);
+  const [player1Active, togglePlayer1] = useState(false);
+  const [player2Active, togglePlayer2] = useState(false);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ClockComponent
+      startTime = "5:00"
+      active={player1Active}
+      color="white"
+      >
+      </ClockComponent>
+      <ClockComponent
+      startTime = "5:00"
+      active={player2Active}
+      color="black"
+      >
+      </ClockComponent>
+    </SafeAreaView>
   );
 }
 
