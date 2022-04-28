b63a768f892572a773b88e90ca461637deafafb6
aidanleuck <aidan@leucks.com>
2022-04-28 16:59:48 -0600

    Added handicap: git push

--- settingsPopup.js
+++ settingsPopup.js
@@ -1,15 +1,38 @@
 import React, {useState} from 'react';
-import { Button, Dialog, Portal, TextInput } from 'react-native-paper';
+import {View, StyleSheet} from 'react-native'
+import { Button, Dialog, Portal, TextInput, Checkbox, Text, Title} from 'react-native-paper';
 
 const SettingsPage = ({visible, setVisible, whiteTime, setWhiteTime, blackTime, setBlackTime}) => {
     const [errors, setErrors] = useState([]);
     const [whiteInput, setWhiteInput] = useState(`${whiteTime}`)
     const [blackInput, setBlackInput] = useState(`${blackTime}`)
+    const [handiCap, setHandiCap] = useState(false);
+
+    const styles = StyleSheet.create({
+      textInput: {
+        marginBottom: 5
+      },
+      checkBoxView:{
+        paddingLeft: 0
+      },
+      title:{
+        textAlign: "center",
+      }
+    });
+
+    let blackInputVisible = handiCap ? {} : {"display" : "none"};
+    let inputTitle = handiCap ? "White Player Time (s)" : "Player Times (s)";
+
     const hideDialog = () => setVisible(false);
     const okayHandler = () => {
         let errors = [];
+        let blackInputCopy = blackInput;
+        if(!handiCap){
+          blackInputCopy = whiteInput;
+        }
+      
         validateTime(whiteInput, setWhiteTime, errors)
-        validateTime(blackInput, setBlackTime, errors)
+        validateTime(blackInputCopy, setBlackTime, errors)
 
         if(errors.length == 0){
             setVisible(false);
@@ -27,22 +50,36 @@ const SettingsPage = ({visible, setVisible, whiteTime, setWhiteTime, blackTime, 
         }
     }
 
+
+
     return (
       <Portal>
         <Dialog visible={visible} onDismiss={hideDialog}>
         <Dialog.Content>
+        <Title style={styles.title}>Game Configuration</Title>
           <TextInput
-          label="White Player Time (s)"
+          style={styles.textInput}
+          label={inputTitle}
           placeholder="Insert time in seconds"
           value={whiteInput}
           onChangeText={text => setWhiteInput(text)}
          />
           <TextInput
+          style={[blackInputVisible, styles.textInput]}
           label="Black Player Time (s)"
           placeholder="Insert time in seconds"
+          disabled={!handiCap}
           value={blackInput}
           onChangeText={text => setBlackInput(text)}
          />
+         <View style={styles.checkBoxView}>
+          <Checkbox.Item 
+           label="Allow Handicap" 
+           status={handiCap ? 'checked' : 'unchecked'} 
+
+           onPress={() => {
+            setHandiCap(!handiCap)}}/>
+        </View>
         </Dialog.Content>
           <Dialog.Actions>
             <Button onPress={() => setVisible(false)}>Cancel</Button>
